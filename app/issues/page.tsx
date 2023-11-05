72fa46f59b911f891e78f06b016b0af9678c4f50
Jordan <jordantseng1024@gmail.com>
2023-11-05 22:46:37 +0800

    Build issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,14 @@
+'use client';
 import React from 'react';
 import { Button } from '@radix-ui/themes';
+import { useRouter } from 'next/navigation';
 
 const IssuesPage = () => {
+  const router = useRouter();
+
   return (
     <div>
-      <Button>wtf</Button>
+      <Button onClick={() => router.push('/issues/new')}>New Issue</Button>
     </div>
   );
 };
