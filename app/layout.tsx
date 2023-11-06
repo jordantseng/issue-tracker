5f5252792945b7dff992a2632267275afb5d4a95
Jordan <jordantseng1024@gmail.com>
2023-11-06 22:40:22 +0800

    Fix radix-ui themes bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <body>
-        <Theme appearance="light" accentColor="violet">
+        <Theme accentColor="violet">
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
