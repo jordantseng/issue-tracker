ccd2cdd28c971d2db687e5715a80ba136cac99a9
Jordan <jordantseng1024@gmail.com>
2023-11-10 10:56:41 +0800

    Add edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,38 +1,56 @@
 import prisma from '@/prisma/client';
-import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import { Pencil2Icon } from '@radix-ui/react-icons';
 import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
 
 import { IssueStatusBadge } from '@/app/components';
+import Link from 'next/link';
 
 type IssueDetailPageProps = {
   params: { id: string };
 };
 
-const IssueDetailPage = async ({ params }: IssueDetailPageProps) => {
-  if (Number.isNaN(parseInt(params.id))) {
+const getIssue = async (id: string) => {
+  if (Number.isNaN(parseInt(id))) {
     notFound();
   }
 
   const issue = await prisma.issue.findUnique({
-    where: { id: parseInt(params.id) },
+    where: { id: parseInt(id) },
   });
 
   if (!issue) {
     notFound();
   }
 
+  return issue;
+};
+
+const IssueDetailPage = async ({ params }: IssueDetailPageProps) => {
+  const issue = await getIssue(params.id);
+
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="3" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.updatedAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt="4">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: '1', md: '2' }} gap="5">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap="3" my="2">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.updatedAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt="4">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Link href={`/issues/${issue.id}/edit`}>
+          <Button>
+            <Pencil2Icon />
+            Edit Issue{' '}
+          </Button>
+        </Link>
+      </Box>
+    </Grid>
   );
 };
 
