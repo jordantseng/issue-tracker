7649ce5f77387f1ad99a7cc831abf850d58f352c
Jordan <jordantseng1024@gmail.com>
2023-11-10 15:21:47 +0800

    Show delete confirmation dialog

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,4 +1,5 @@
-import { Button } from '@radix-ui/themes';
+'use client';
+import { AlertDialog, Button, Flex } from '@radix-ui/themes';
 import Link from 'next/link';
 
 type DeleteIssueButtonProps = {
@@ -7,11 +8,29 @@ type DeleteIssueButtonProps = {
 
 const DeleteIssueButton = ({ issueId }: DeleteIssueButtonProps) => {
   return (
-    <Link href={`/issues/${issueId}/edit`}>
-      <Button className="w-full" color="red">
-        Delete Issue
-      </Button>
-    </Link>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue?
+        </AlertDialog.Description>
+        <Flex gap="3" mt="4">
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="soft">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red" variant="soft">
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
